28f5561633ee3ef40e6577c7f4b58e929dcfe3e2
Seyha Uong <uong.seyha@gmail.com>
2026-01-16 12:12:26 -0500

    First setup Angular app

---
+++ Client/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig, provideZoneChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
+  ]
+};
